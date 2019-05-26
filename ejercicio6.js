082d53d6a907d2ca8aeab0e79f5537dfe50ffa1e
Victor Llamas Olivares <victorllamas@gmail.com>
2019-05-26 17:11:05 -0500

    input values modified

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,6 +1,6 @@
 const shot1 = parseInt("1")
-const shot2 = parseInt("1")
-const shot3 = parseInt("2")
+const shot2 = parseInt("2")
+const shot3 = parseInt("3")
 if(shot1 === shot2 || shot1 === shot3 || shot2 === shot3){
     console.log("Ya Ganaste")
 }else{
